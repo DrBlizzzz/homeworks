0f25661125f42b8fbe7f9e722e8beb5e10f428da
DrBlizzzz <kira_deus@mail.ru>
2023-03-12 11:17:57 +0300

    HW07 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,31 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	// этот пайп для передачи канала от одного стейджа к другому
+	readyChannels := make(chan Out, 1)
+	// этот пайп выводится если сработал done
+	terminateChannel := make(Bi)
+	// заранее закрываем пайпы
+	defer close(readyChannels)
+	defer close(terminateChannel)
+	for _, stage := range stages {
+		select {
+		case pipe := <-readyChannels:
+			readyChannels <- stage(pipe)
+		case <-done:
+			return terminateChannel
+		// здесь будет первое вхождение
+		default:
+			readyChannels <- stage(in)
+		}
+	}
+	// после данного цикла все пайпы были прокинуты в нужные стейджи
+	// перестрахуемся и сделаем еще один select(на тот случай если done все же пришел под конец)
+	// это имеет смысл делать
+	select {
+	case <-done:
+		return terminateChannel
+	case output := <-readyChannels:
+		return output
+	}
 }
